724cb1da2cd031f56cdbbea7270690402b6c9262
trander <thevinh15925@gmail.com>
2025-12-23 20:47:36 +0700

    fix

--- student-exercises/app/layout.tsx
+++ student-exercises/app/layout.tsx
@@ -8,7 +8,11 @@ export const metadata = {
   description: 'Comprehensive exercises for Next.js Advanced Framework',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
